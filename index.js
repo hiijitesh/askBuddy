56e480e48daa1bfd22fbc977ae3ea114b2e9f512
Jitesh Kumar <jiteshece@gmail.com>
2023-12-13 22:13:13 +0530

    minor fixes

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const route = require("./routes");
 const { isAuthenticated } = require("./utils/auth");
 
 const app = express();
-app.use("/", (req, res) => {
-    res.send("<h1> ASK ME</h1>");
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
 app.use(isAuthenticated, route);
+
+app.use("/", (req, res) => {
+    res.send("<h1> ASK ME</h1>");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on the port ${PORT} ✅✅✅`);
